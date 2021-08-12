ec2f3f745dc595a5753c3b888b9603e929f59c21
SiennaRose <siennarose.sacramento@gmail.com>
2021-08-12 07:31:37 -0700

    added MoveToNextLevel() in BotKillerAgent.cs and added a call to MoveToNextLevel() ub OnEpisodeBegin() in BotKillerAgent.cs

--- Assets/FPS/Scripts/Gameplay/Nautica/BotKillerAgent.cs
+++ Assets/FPS/Scripts/Gameplay/Nautica/BotKillerAgent.cs
@@ -63,6 +63,12 @@ namespace Nautica
 			// DANGER: this assumes agent is nested under the level prefab
 			// TrainingManager shuffles agents around to different levels, so need to be careful
 			var trainingLevelManager = transform.parent.GetComponent<TrainingLevelManager>();
+
+			if (trainingLevelManager.AllEnemiesAreDead())
+			{
+				MoveToNextLevel(trainingLevelManager);
+			}
+
 			if (!trainingLevelManager)
 			{
 				Debug.unityLogger.Log(LOGTAG, "Could not find TrainingLevelManager!");
@@ -74,6 +80,13 @@ namespace Nautica
 			trackedHealth = 1f;
 			trackedEnemyHealth.Clear();
 			foreach (var e in enemies) trackedEnemyHealth.Add(1f);
+		}
+
+		private void MoveToNextLevel(TrainingLevelManager trainingLevelManager)
+		{
+			GameObject challengeManager = GameObject.Find("ChallengeManager");
+			var trainingManager = challengeManager.GetComponent<TrainingManager>();
+			trainingManager.SetUpNextLevel();
 		}
 
 		private void DebugLogReward(float reward, string rewardtype)
